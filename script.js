20b05142ebb897029afdc0b589a61303c1dcdaac
codinome <146401049+codinomello@users.noreply.github.com>
2025-01-27 12:47:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function calcular() {
   // captura os valores dos campos de entrada
   const campos = {
-    NP: parseFloat(document.getElementById('NP').value) || 0,
     PD: parseFloat(document.getElementById('PD').value) || 0,
+    NP: parseFloat(document.getElementById('NP').value) || 0,
     SI: parseFloat(document.getElementById('SI').value) || 0,
     PO1: parseFloat(document.getElementById('PO1').value) || 0,
     PO2: parseFloat(document.getElementById('PO2').value) || 0,
-    PO3: parseFloat(document.getElementById('PO3').value) || 0,
-    EX: parseFloat(document.getElementById('EX').value) || 0,
-    DS: parseFloat(document.getElementById('DS').value) || 0,
     PV: parseFloat(document.getElementById('PV').value) || 0,
+    DS: parseFloat(document.getElementById('DS').value) || 0,
+    EX: parseFloat(document.getElementById('EX').value) || 0,
     PE: parseFloat(document.getElementById('PE').value) || 0,
     MP: parseFloat(document.getElementById('MP').value) || 0,
-    ED: parseFloat(document.getElementById('ED').value) || 0,
     PR: parseFloat(document.getElementById('PR').value) || 0,
+    ED: parseFloat(document.getElementById('ED').value) || 0,
   }
 
   const nível = document.getElementById('nível-educacional').value
@@ -26,15 +25,15 @@ function calcular() {
   // verifica o nível educacional e calcula a nota e recuperação
   switch (nível) {
     case 'EFI':
-      nota = campos.NP * 0.20 + campos.PD * 0.25 + campos.SI * 0.25
+      nota = campos.NP * 0.30 + campos.PD * 0.40 + campos.SI * 0.10
       recuperação = (nota + (campos.ED * 0.30 + campos.PR * 0.70)) / 2
       break
     case 'EFII':
-      nota = campos.NP * 0.20 + campos.PD * 0.25 + campos.SI * 0.25 + ((campos.PO1 + campos.PO2) * 0.30 / 2)
+      nota = campos.NP * 0.30 + campos.PD * 0.40 + campos.SI * 0.10 + ((campos.PO1 + campos.PO2 + campos.SI) * 0.30 / 2)
       recuperação = (nota + (campos.ED * 0.30 + campos.PR * 0.70)) / 2
       break
     case 'EM':
-      nota = campos.NP * 0.20 + campos.PD * 0.25 + campos.SI * 0.25 + ((campos.PO1 + campos.PO2 + campos.PO3) * 0.30 / 3)
+      nota = campos.NP * 0.30 + campos.PD * 0.40 + ((campos.PO1 + campos.PO2 + campos.SI) * 0.30 / 3)
       recuperação = (campos.MP + (campos.ED * 0.30 + campos.PR * 0.70)) / 2
       técnico = campos.EX * 0.20 + campos.DS * 0.30 + campos.PV * 0.50
       projeto = campos.PE
@@ -81,7 +80,7 @@ function calcular() {
       resultado.classList.remove('bg-purple-400')
       resultado.classList.remove('bg-teal-500')
       const abaixo = 6 - nota
-      mensagem += `Que pena! Você não passou. <br>Sua nota está <b>${abaixo.toFixed(1)}</b> pontos abaixo da média.`
+      mensagem += `Quase! Você não passou. <br>Sua nota está <b>${abaixo.toFixed(1)}</b> pontos abaixo da média.`
     } else {
       alert("A nota final não pôde ser calculada.") // induz que a nota é um número inválido
       return 
@@ -121,7 +120,7 @@ function calcular() {
       resultado.classList.remove('bg-purple-400')
       resultado.classList.remove('bg-teal-500')
       const abaixo = 6 - técnico
-      mensagem += `Que pena! Você não passou. <br>Sua nota está <b>${abaixo.toFixed(1)}</b> pontos abaixo da média.`
+      mensagem += `Quase! Você não passou. <br>Sua nota está <b>${abaixo.toFixed(1)}</b> pontos abaixo da média.`
     } else {
       alert("A nota final não pôde ser calculada.") // induz que a nota é um número inválido
       return 
@@ -161,7 +160,7 @@ function calcular() {
       resultado.classList.remove('bg-purple-400')
       resultado.classList.remove('bg-teal-500')
       const abaixo = 6 - recuperação
-      mensagem += `Que pena! Você não passou. <br>Sua nota está <b>${abaixo.toFixed(1)}</b> pontos abaixo da média.`
+      mensagem += `Quase! Você não passou. <br>Sua nota está <b>${abaixo.toFixed(1)}</b> pontos abaixo da média.`
     } else {
       alert("A nota final não pôde ser calculada.") // induz que a nota é um número inválido
       return 
@@ -184,16 +183,8 @@ function mostrar() {
     document.getElementById('técnico').classList.add('hidden')
     document.getElementById('técnico-seção').classList.add('hidden')
     document.getElementById('recuperação-seção').classList.add('hidden')
-    document.getElementById('ações').classList.add('hidden')
-    document.getElementById('prova-objetiva').classList.add('hidden')
-    document.getElementById('PO3').classList.add('hidden')
-  } 
-  else if (nível === "EFII") {
-    document.getElementById('formulário').classList.remove('hidden')
-    document.getElementById('regular-seção').classList.add('hidden')
-    document.getElementById('técnico').classList.add('hidden')
-    document.getElementById('técnico-seção').classList.add('hidden')
-    document.getElementById('recuperação-seção').classList.add('hidden')
+    document.getElementById('projeto').classList.add('hidden')
+    document.getElementById('projeto-seção').classList.add('hidden')
     document.getElementById('ações').classList.add('hidden')
     document.getElementById('prova-objetiva').classList.remove('hidden')
     const PO1 = document.getElementById('PO1-label')
@@ -202,15 +193,32 @@ function mostrar() {
     const PO2 = document.getElementById('PO2-label')
     PO2.innerHTML = "Prova Objetiva 2 (15%)"
     document.getElementById('PO2-label').classList.remove('hidden')
-    document.getElementById('PO3-label').classList.add('hidden')
-    document.getElementById('PO3').classList.add('hidden')
+  } 
+  else if (nível === "EFII") {
+    document.getElementById('formulário').classList.remove('hidden')
+    document.getElementById('regular-seção').classList.add('hidden')
+    document.getElementById('técnico').classList.add('hidden')
+    document.getElementById('técnico-seção').classList.add('hidden')
+    document.getElementById('recuperação-seção').classList.add('hidden')
+    document.getElementById('projeto').classList.add('hidden')
+    document.getElementById('projeto-seção').classList.add('hidden')
+    document.getElementById('ações').classList.add('hidden')
+    document.getElementById('prova-objetiva').classList.remove('hidden')
+    const PO1 = document.getElementById('PO1-label')
+    PO1.innerHTML = "Prova Objetiva 1 (15%)"
+    document.getElementById('PO1-label').classList.remove('hidden')
+    const PO2 = document.getElementById('PO2-label')
+    PO2.innerHTML = "Prova Objetiva 2 (15%)"
+    document.getElementById('PO2-label').classList.remove('hidden')
   } 
   else if (nível === "EM") {
     document.getElementById('formulário').classList.remove('hidden')
     document.getElementById('regular-seção').classList.add('hidden')
-    document.getElementById('recuperação-seção').classList.add('hidden')
     document.getElementById('técnico').classList.remove('hidden')
     document.getElementById('técnico-seção').classList.add('hidden')
+    document.getElementById('recuperação-seção').classList.add('hidden')
+    document.getElementById('projeto').classList.add('hidden')
+    document.getElementById('projeto-seção').classList.add('hidden')
     document.getElementById('ações').classList.add('hidden')
     document.getElementById('prova-objetiva').classList.remove('hidden')
     const PO1 = document.getElementById('PO1-label')
@@ -219,9 +227,6 @@ function mostrar() {
     const PO2 = document.getElementById('PO2-label')
     PO2.innerHTML = "Prova Objetiva 2 (10%)"
     PO2.classList.remove('hidden')
-    const PO3 = document.getElementById('PO3-label')
-    PO3.classList.remove('hidden')
-    document.getElementById('PO3').classList.remove('hidden')
   }
 }
 
@@ -232,9 +237,11 @@ function menu(display) {
       document.getElementById('regular').classList.add('selecionado')
       document.getElementById('técnico').classList.remove('selecionado')
       document.getElementById('recuperação').classList.remove('selecionado')
+      document.getElementById('projeto').classList.remove('hidden')
       document.getElementById('regular-seção').classList.remove('hidden')
       document.getElementById('técnico-seção').classList.add('hidden')
       document.getElementById('recuperação-seção').classList.add('hidden')
+      document.getElementById('projeto-seção').classList.add('hidden')
       document.getElementById('ações').classList.remove('hidden')
       break;    
     case 2:
@@ -242,9 +249,11 @@ function menu(display) {
       document.getElementById('técnico').classList.add('selecionado')
       document.getElementById('regular').classList.remove('selecionado')
       document.getElementById('recuperação').classList.remove('selecionado')
+      document.getElementById('projeto').classList.remove('hidden')
       document.getElementById('regular-seção').classList.add('hidden')
       document.getElementById('técnico-seção').classList.remove('hidden')
       document.getElementById('recuperação-seção').classList.add('hidden')
+      document.getElementById('projeto-seção').classList.add('hidden')
       document.getElementById('ações').classList.remove('hidden')
       break;
     case 3:
@@ -252,9 +261,23 @@ function menu(display) {
       document.getElementById('recuperação').classList.add('selecionado')
       document.getElementById('regular').classList.remove('selecionado')
       document.getElementById('técnico').classList.remove('selecionado')
+      document.getElementById('projeto').classList.remove('hidden')
       document.getElementById('regular-seção').classList.add('hidden')
       document.getElementById('técnico-seção').classList.add('hidden')
       document.getElementById('recuperação-seção').classList.remove('hidden')
+      document.getElementById('projeto-seção').classList.add('hidden')
+      document.getElementById('ações').classList.remove('hidden')
+      break;
+    case 4:
+      // exibe as opções para a recuperação
+      document.getElementById('recuperação').classList.remove('selecionado')
+      document.getElementById('regular').classList.remove('selecionado')
+      document.getElementById('técnico').classList.remove('selecionado')
+      document.getElementById('projeto').classList.remove('hidden')
+      document.getElementById('regular-seção').classList.add('hidden')
+      document.getElementById('técnico-seção').classList.add('hidden')
+      document.getElementById('recuperação-seção').classList.add('hidden')
+      document.getElementById('projeto-seção').classList.remove('hidden')
       document.getElementById('ações').classList.remove('hidden')
       break;
     default:
@@ -263,7 +286,7 @@ function menu(display) {
 }
 
 function remover() {
-  const campos = ['NP', 'PD', 'SI', 'PO1', 'PO2', 'PO3', 'EX', 'PV', 'PE', 'ED', 'PR']
+  const campos = ['PD', 'NP', 'SI', 'PO1', 'PO2', 'PV', 'DS', 'EX', 'PE', 'MP', 'PR', 'ED']
 
   // limpa campos de texto
   campos.forEach((id) => {
@@ -284,7 +307,7 @@ function remover() {
   })
 
   // limpa o campo <select>
-  const select = {PO1: document.getElementById('PO1'), PO2: document.getElementById('PO2'), PO3: document.getElementById('PO3')}
+  const select = {PO1: document.getElementById('PO1'), PO2: document.getElementById('PO2')}
   if (select) {
     select.value = null // define o valor padrão
   }
